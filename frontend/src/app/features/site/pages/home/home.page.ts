6e6c1709ae40e3d694242cbc8f67a85368ec26b6
lesquel <lesquel1319@gmail.com>
2025-05-15 18:42:15 -0500

    a

--- frontend/src/app/features/site/pages/home/home.page.ts
+++ frontend/src/app/features/site/pages/home/home.page.ts
@@ -1,12 +1,10 @@
 import { Component, inject } from "@angular/core";
 import { toSignal } from "@angular/core/rxjs-interop"; 
-import { MatIcon } from "@angular/material/icon";
 import { UseClientSiteService } from "../../services";
 import { UseClientProductService } from "@features/product/services";
-import { GallaryImageProductCardComponent } from "@features/product/components";
 import { UseClientCategoryProductsService } from "@features/category-products/services";
-import { CardCartegoryProductsComponent } from "@features/category-products/components";
 import { HeroComponent } from "../../../../shared/components/hero/hero.component"; 
+import { menuRoutesConfig } from "@features/menu/config";
 
 
 @Component({
@@ -22,6 +20,7 @@ export class HomePage {
     readonly aboutUs = toSignal(this.useClientSiteService.getAboutUs());
     readonly products = toSignal(this.useClientProductService.getFavoriteProducts());
     readonly categoryProducts = toSignal(this.useClientCategoryProductsService.getCategoryProducts());
+    readonly menuRoutesConfig = menuRoutesConfig;
     
     isAboutUsVideoPlaying = false; //  Controlamos si el video ya se reprodujo o no
 
--- frontend/src/app/shared/components/hero/hero.component.ts
+++ frontend/src/app/shared/components/hero/hero.component.ts
@@ -1,12 +1,14 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Hero } from '@shared/models';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './hero.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeroComponent {
   hero = input.required<Hero>();
+  link = input.required<string>();
 }
