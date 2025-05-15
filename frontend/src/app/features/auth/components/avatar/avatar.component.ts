f28ec9f0abafbe2ca094335111841a68ba89562d
lesquel <lesquel1319@gmail.com>
2025-05-14 21:49:32 -0500

    aaa

--- frontend/src/app/features/auth/components/avatar/avatar.component.ts
+++ frontend/src/app/features/auth/components/avatar/avatar.component.ts
@@ -1,8 +1,12 @@
 import { ChangeDetectionStrategy, Component} from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-avatar',
   templateUrl: './avatar.component.html',
+  imports: [
+    MatIconModule,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AvatarComponent {
