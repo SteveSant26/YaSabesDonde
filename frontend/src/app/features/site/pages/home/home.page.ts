e414cd9807f31d9c9972e6687d92a4a41bd4f91f
SteveSant26 <Bryanmenoscal2005@gmail.com>
2025-05-14 21:03:59 -0500

    .

--- frontend/src/app/features/site/pages/home/home.page.ts
+++ frontend/src/app/features/site/pages/home/home.page.ts
@@ -12,7 +12,7 @@ import { HeroComponent } from "../../../../shared/components/hero/hero.component
 @Component({
     selector: "home-page",
     templateUrl: "./home.page.html",
-    imports: [GallaryImageProductCardComponent, CardCartegoryProductsComponent, HeroComponent],
+    imports: [HeroComponent],
 })
 export class HomePage {
     private useClientSiteService = inject(UseClientSiteService);
@@ -24,6 +24,10 @@ export class HomePage {
     readonly categoryProducts = toSignal(this.useClientCategoryProductsService.getCategoryProducts());
     
     isAboutUsVideoPlaying = false; //  Controlamos si el video ya se reprodujo o no
+
+    ngOnInit(){
+      console.log(this.categoryProducts()?.data)
+    }
 
     playAboutUsVideo(video: HTMLVideoElement) {
       if (video.paused) {
